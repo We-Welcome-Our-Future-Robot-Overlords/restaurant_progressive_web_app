1026e24b4733c1d2ab053aede64112503e4d633e
Jack Han <jcdhan1@sheffield.ac.uk>
2018-04-13 18:39:21 +0100

    Searching: restaurants within a given radius from given coordinates.

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -14,11 +14,28 @@ function clean(obj) {
 exports.retrieve = function (req, res) {
     var rstrntData = req.body;
     clean(rstrntData);
+    var locus = {};
+    console.log("lat 2 is", lat2);
+    if ('lat' in rstrntData && 'lng' in rstrntData) {
+        var lat2 = rstrntData.lat;
+        var lng2 = rstrntData.lng;
+        var radius = rstrntData.radius;
+        locus = {$where: function() {
+                var toreturn  = haversine(this.lat, lat2, this.lng, lng2) < radius;
+                console.log(toreturn);
+                return toreturn;
+            }
+        };
+    }
+    delete rstrntData['lat'];
+    delete rstrntData['lng'];
+    delete rstrntData['radius'];
     if (rstrntData == null || Object.keys(rstrntData).length == 0) {
         res.status(403).send('No data sent!');
     } else {
         try {
-            Restaurant.find(rstrntData,
+            console.log("locus is now:", locus);
+            Restaurant.find({$and: [rstrntData, locus]},
                 function (err, restaurants) {
                     if (err) {
                         res.status(500).send('Invalid data!');
