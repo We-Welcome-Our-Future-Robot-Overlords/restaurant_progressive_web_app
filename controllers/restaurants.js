4cdbf3f2908650829c7e5e1904dc8479504814c7
Jack Han <jcdhan1@sheffield.ac.uk>
2018-04-13 02:00:21 +0100

    Searching: Allowing partial matches and more than one result

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,28 +1,34 @@
 var Restaurant = require('../models/restaurants');
 
 /* TODO try some query to see if database work as normal */
-exports.retrieve = function (req, res) {
-    console.log("foo bar");
-    var rstrntData = req.body;
-    if (rstrntData == null) {
-        res.status(403).send('No data sent!')
+
+//Remove empty attributes:
+function clean(obj) {
+    for (var propName in obj) {
+        if (obj[propName] === null || obj[propName] === undefined || obj[propName] === '') {
+            delete obj[propName];
+        }
     }
-    try {
-        Restaurant.find({name: rstrntData.req_name, cuisine: rstrntData.req_cuisine},
-            function (err, restaurants) {
-                if (err)
-                    res.status(500).send('Invalid data!');
-                var restaurant = null;
-                if (restaurants.length > 0) {
-                    var firstElem = restaurants[0];
-                    restaurant = {
-                        res_name: firstElem.name, res_cuisine: firstElem.cuisine
-                    };
-                }
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(restaurant));
-            });
-    } catch (e) {
-        res.status(500).send('error ' + e);
+}
+
+exports.retrieve = function (req, res) {
+    var rstrntData = req.body;
+    clean(rstrntData);
+    if (rstrntData == null || Object.keys(rstrntData).length == 0) {
+        res.status(403).send('No data sent!');
+    } else {
+        try {
+            Restaurant.find(rstrntData,
+                function (err, restaurants) {
+                    if (err) {
+                        res.status(500).send('Invalid data!');
+                    } else {
+                        res.setHeader('Content-Type', 'application/json');
+                        res.send(JSON.stringify(restaurants));
+                    }
+                });
+        } catch (e) {
+            res.status(500).send('error ' + e);
+        }
     }
 }
