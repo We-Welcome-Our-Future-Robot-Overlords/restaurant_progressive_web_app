639cc0e256f69261caf139337d5ca5cec72a9fea
ACA15JCH <jcdhan1@sheffield.ac.uk>
2018-04-16 06:06:17 +0100

    Using selectize instead since it works on mobile

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist')); // redirect bootstrap
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/chosen', express.static(__dirname + '/node_modules/chosen-js')); // redirect chosen
+app.use('/selectize', express.static(__dirname + '/node_modules/selectize/dist')); // redirect selectize
 
 app.use('/', index);
 app.use('/users', users);
